4b6b975cc7bf07626792adde7f01a86d91228667
Johnny Murillo <jossters@gmail.com>
2022-02-03 23:29:48 -0800

    added useEffect

--- src/components/singleTodo.tsx
+++ src/components/singleTodo.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { Todo } from './model';
 import { AiFillEdit, AiFillDelete } from "react-icons/ai";
 import { MdDone } from "react-icons/md";
@@ -38,13 +38,21 @@ const SingleTodo = ({ todo, todos, setTodos }: Props) => {
 
   };
 
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    inputRef.current?.focus()
+  }, [edit]);
+  
+
   return (
     <form className="todos__single" onSubmit={(e) => handleEdit(e,todo.id)}>
       {edit ? (
         <input 
+        ref={inputRef}
         value={editTodo} 
         onChange={(e) => setEditTodo(e.target.value)}
-        className='todos__single__test' 
+        className='todos__single__text' 
         />
       ): todo.isDone ? (
         <s className="todos__single__text">{todo.todo}</s>
