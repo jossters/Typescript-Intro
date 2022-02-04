4afc3f6aa09635084028c251e75ef11a833a9d89
Johnny Murillo <jossters@gmail.com>
2022-02-03 23:20:07 -0800

    added delet strike edit

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import InputField from './components/InputField';
+import { Todo } from './components/model';
+import TodoList from './components/TodoList';
 
 // let name: string;
 // let age: number | string;
@@ -21,12 +23,34 @@ import InputField from './components/InputField';
 
 
 const App: React.FC = () => {
+  const [todo, setTodo] = useState<string>("");
+  const [todos, setTodos] = useState<Todo[]>([]);
+  
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if (todo) {
+      setTodos([...todos,{id: Date.now(),todo:todo,isDone: false}])
+      setTodo("");
+    }
+  };
+
+  console.log(todos)
+
   return (
-    <div className="App">
+    <div className="App"> 
       <span className='heading'>Task</span>
-      <InputField/>
+      <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd}/>
+      <TodoList todos={todos} setTodos={setTodos} />
+      
     </div>
   );
 }      
 
 export default App;
+
+
+
+
+
+
